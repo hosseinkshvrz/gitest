63793bd707986cbbe3629529133d6abe15c63386
Hossein Keshavarz <hosseinkeshavarz1997@gmail.com>
2022-01-18 15:39:31 -0500

    remove files

--- src/file_generator.py
+++ src/file_generator.py
@@ -1,8 +1,11 @@
 from pydriller import Git
 
 repo = Git('../repos/bookkeeper')
-commit = repo.get_commit('a65e888b25eb966921ff39032e04f367a553d634')
+commit = repo.get_commit('370d785ff674841943fa8cddee1964408e826f5b')
+lines = 0
 for m in commit.modified_files:
     if m.source_code_before:
+        lines += m.source_code.count('\n')
         with open('../files/{}'.format(m.filename), 'w') as fp:
             fp.write(m.source_code_before)
+print(lines)
