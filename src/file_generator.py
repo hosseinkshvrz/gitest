ba0256398306ce79e54b6a2dfaaaaa169278a2ee
Hossein Keshavarz <hosseinkeshavarz1997@gmail.com>
2022-01-19 19:36:53 -0500

    test 1 again

--- src/file_generator.py
+++ src/file_generator.py
@@ -1,7 +1,7 @@
 from pydriller import Git
 
 repo = Git('../repos/bookkeeper')
-commit = repo.get_commit('370d785ff674841943fa8cddee1964408e826f5b')
+commit = repo.get_commit('f737e9b')
 lines = 0
 for m in commit.modified_files:
     if m.source_code_before:
