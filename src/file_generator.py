38ec2edc0c4c3f21a2ccd8c41cb20fc97ff33307
Hossein Keshavarz <hosseinkeshavarz1997@gmail.com>
2022-01-19 20:31:29 -0500

    camel test 3 model 30%

--- src/file_generator.py
+++ src/file_generator.py
@@ -1,11 +1,11 @@
 from pydriller import Git
 
 repo = Git('../repos/camel')
-commit = repo.get_commit('363a813')
+commit = repo.get_commit('1ef8402')
 lines = 0
 for m in commit.modified_files:
     if m.source_code_before:
-        lines += m.source_code.count('\n')
+        # lines += m.source_code.count('\n')
         with open('../files/{}'.format(m.filename), 'w') as fp:
             fp.write(m.source_code_before)
 print(lines)
